f0f4774a2625244658fa8649368e892c2cfd6f30
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-08-23 15:23:39 -0700

    first commit

---
+++ routes/hotels.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel, getHotelRooms } from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+
+//DELETE
+router.put("/:id", verifyAdmin, deleteHotel);
+
+//GET
+router.get("/find/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
+
+export default router
